cfd4c0fd9c5cf20dedd76a9a64436829c565c364
thetoolman <thetoolman@6bd413e4-bbd7-11dd-bed2-db724ec49875>
2009-11-06 08:58:18 +0000

    backfill from distributable zips

---
+++ tags/sexy-combo-2.0.5/examples/usage.js
@@ -0,0 +1,34 @@
+$(function() {
+    $("#basic-combo").sexyCombo();
+    
+    $("#empty-combo").sexyCombo({emptyText: "Choose a state..."});
+    
+    $("#autofill-combo").sexyCombo({autoFill: true});
+    
+    $("#selected-combo").sexyCombo({triggerSelected: true});
+    
+    $("#up-combo").sexyCombo({dropUp: true});
+    
+    $("#filter-combo").sexyCombo({filterFn: function() {
+        return true;
+    }, dropUp: true});
+    
+    $("#mixed-combo").sexyCombo({emptyText: "Choose a state", autoFill: true, dropUp: true});
+    
+    var data = [];
+    $("#selectbox").children().each(function() {
+        var $this = $(this);
+        data.push({value: $this.attr("value"), text: $this.text()});
+    });
+    
+    $.sexyCombo.create({name: "static-combo", id: "static-combo", container: "#static-container", data: data, dropUp: true});
+    
+    data[0].selected = true;
+    $.sexyCombo.create({name: "static-selected-combo", id: "static-selected-combo", container: "#static-selected", data: data, dropUp: true, triggerSelected: true});
+    
+    $.sexyCombo.create({name: "ajax-combo", id: "ajax-combo", container: "#ajax-container", url: "example.json", dropUp: true});
+    
+    $("#multiple-combo").sexyCombo({dropUp: true});
+
+
+});
